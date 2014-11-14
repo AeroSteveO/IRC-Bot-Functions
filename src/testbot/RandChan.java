a1fd802ed0819618cbf175cc88ffb9751e737979
AeroSteveO <thesteveo64@gmail.com>
2014-11-13 19:19:03 -0500

    implemented throttle object in randchan, added some extra features, and updated to latest revision

--- src/testbot/RandChan.java
+++ src/testbot/RandChan.java
@@ -7,10 +7,10 @@
 package testbot;
 
 import Objects.KeyFinder;
+import Objects.Throttle;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.util.Date;
 import java.util.LinkedList;
 import org.pircbotx.hooks.ListenerAdapter;
 import org.pircbotx.hooks.events.MessageEvent;
@@ -20,6 +20,7 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.pircbotx.Colors;
 
 /**
  *
@@ -31,59 +32,83 @@ import org.json.simple.parser.ParseException;
  * Requirements:
  * - APIs
  *    JSON-Simple-1.1.1
- * - Custom Classes
- *    N/A
- *
+ * - Custom Objects
+ *    KeyFinder
+ *    Throttle
+ * - Linked Classes
+ *    Global
+ * 
  * Activate Commands with:
  *      !randchan [board]
  *          responds with a link to a random 4chan image in the given board,
  *          if no board is given, then it responds with a random image from a
  *          random board
+ *      !set rcall
+ *          Changes the number of randchan calls that can be made per specified 
+ *          amount of time, if no value is input, gives the current settings
+ *      !set rtime
+ *          Changes the amount of time between randchan call flushes, randchan 
+ *          is stopped when # of calls > rcall over the course of rtime, if 
+ *          no value is input, gives the current settings
+ * 
  */
 public class RandChan extends ListenerAdapter {
     
     private LinkedList<Long> timeLog = new LinkedList<Long>();
-    private static final int MAX_LOG = 3;
-    private static final long MAX_TIME = 30*1000;
-    List<String> boardList = getBoardList();
+    private int maxLog = 2;
+    private long maxTime = 100*1000;
+    private List<String> boardList = getBoardList();
+    private List<String> boardTitles = getBoardTitles();
+    private Throttle rThrottle = new Throttle("randchan");
+    boolean setup = setupThrottle(maxLog,maxTime);
     
     @Override
     public void onMessage(MessageEvent event) throws Exception {
+        String message = Colors.removeFormattingAndColors(event.getMessage().trim());
         try{
-            if(event.getMessage().trim().matches("!randchan(\\s+\\p{Alnum}+)?")) {
-                
-                //Little bit of throttling up in here
-                Date d = new Date();
-                long currentTime = d.getTime();
-                if(timeLog.size() > MAX_LOG) {
-                    while(timeLog.size()>0 && currentTime - timeLog.getLast() > MAX_TIME) {
-                        timeLog.pollLast();
-                    }
-                    if(timeLog.size()>MAX_LOG) {
-                        event.getChannel().send().kick( event.getUser(), "DIAF");
-                        return;
-                    }
+            if (message.equalsIgnoreCase("!randchan dict")||message.equalsIgnoreCase("!list boards")||message.equalsIgnoreCase("!randchan list")){
+                String boards = Colors.RED+boardList.get(0)+": "+Colors.NORMAL+boardTitles.get(0)+", ";
+                for(int i=1;i<boardList.size()-1;i++){
+                    boards = boards+Colors.RED+boardList.get(i)+": "+Colors.NORMAL+boardTitles.get(i)+", ";
                 }
-                //Russian roulette up in here
-                if(((int)(Math.random()*6))==0) {
-                    event.getChannel().send().kick(event.getUser(), "No soup for you");
-                    return;
-                }
-                String[] splitString = event.getMessage().split("\\s+");
-                if(splitString.length>1) {
-                    if(boardList.contains(splitString[1])) {
-                        timeLog.addFirst(d.getTime());
-                        event.respond(get4ChanImage(splitString[1]));
+                event.getBot().sendIRC().message(event.getUser().getNick(),boards);
+            }
+            else if(message.toLowerCase().matches("!randchan(\\s+\\p{Alnum}+)?")) {
+                if(!rThrottle.isThrottleActive()){
+                    String[] splitString = event.getMessage().split("\\s+");
+                    if(splitString.length>1) {
+                        if(boardList.contains(splitString[1])) {
+                            event.respond(get4ChanImage(splitString[1]));
+                        }
+                        else {
+                            event.getBot().sendIRC().notice(event.getUser().getNick(), "Board is not allowed/Does not exist");
+                        }
                     }
                     else {
-                        event.getChannel().send().kick(event.getUser(), "Die");
+                        event.respond(get4ChanImage(boardList.get((int) (Math.random()*boardList.size()-1)).toString()));
                     }
-                    get4ChanImage(splitString[1]);
+                }else{
+                    event.getBot().sendIRC().notice(event.getUser().getNick(), "Current number of randchan calls are greater than the rate limiting system allows");
                 }
-                else {
-                    timeLog.addFirst(d.getTime());
-                    event.respond(get4ChanImage(boardList.get((int) (Math.random()*boardList.size()-1)).toString()));
-                }
+            }
+            
+            if (message.toLowerCase().matches("!set rcall [0-9]*")&&(event.getUser().getNick().equalsIgnoreCase(Global.botOwner)||event.getUser().getNick().equalsIgnoreCase("theDoctor"))&&event.getUser().isVerified()){
+                maxLog = Integer.parseInt(message.split(" ")[2])-1;
+                long sec = maxTime/1000;
+                rThrottle.setMaxLog(maxLog);
+                event.getBot().sendIRC().notice(event.getUser().getNick(), Integer.toString(maxLog+1)+" calls can now be made per every "+sec+"s");
+            }
+            
+            if (message.toLowerCase().matches("!set rtime [0-9]*")&&(event.getUser().getNick().equalsIgnoreCase(Global.botOwner)||event.getUser().getNick().equalsIgnoreCase("theDoctor"))&&event.getUser().isVerified()){
+                maxTime = Integer.parseInt(message.split(" ")[2])*1000;
+                long sec = maxTime/1000;
+                rThrottle.setMaxTime(maxTime);
+                event.getBot().sendIRC().notice(event.getUser().getNick(), Integer.toString(maxLog+1)+" calls can now be made per every "+sec+"s");
+            }
+            
+            if (message.equalsIgnoreCase("!set rcall")||message.equalsIgnoreCase("!set rtime")){
+                long sec = maxTime/1000;
+                event.getBot().sendIRC().notice(event.getUser().getNick(), Integer.toString(maxLog+1)+" calls can now be made per every "+sec+"s");
             }
         }
         catch(Exception ex){
@@ -130,6 +155,27 @@ public class RandChan extends ListenerAdapter {
         return(boards);
     }
     
+    
+    //Gets a full list of 4Chan board titles using the 4chan json
+    private List<String> getBoardTitles(){
+        JSONParser parser = new JSONParser();
+        List<String> titles = new ArrayList<>();
+        try{
+            JSONObject jsonObject = (JSONObject) parser.parse(readUrl("http://a.4cdn.org/boards.json"));
+            JSONArray boardsTemp = (JSONArray) jsonObject.get("boards");
+            for (int i=0; i<boardsTemp.size(); i++) {
+                jsonObject = (JSONObject) parser.parse(boardsTemp.get(i).toString());
+                titles.add((String) jsonObject.get("title"));
+            }
+        }
+        catch(Exception ex){
+            ex.printStackTrace();
+            System.out.println(ex.getMessage());
+        }
+        return(titles);
+    }
+    
+    
     private String get4ChanImage(String board) throws Exception {
         String image = new String();
         List<String> threads = new ArrayList<>();
@@ -170,5 +216,10 @@ public class RandChan extends ListenerAdapter {
             }
         }
         return(matchedJson);
+    }
+    private boolean setupThrottle(int maxLog, long maxTime) {
+        rThrottle.setMaxLog(maxLog);
+        rThrottle.setMaxTime(maxTime);
+        return(true);
     }
 }
